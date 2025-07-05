8db440c0fee6582ab8e443f97c902429d5609388
Prateek Mhatre <117933472+Prateek-1920@users.noreply.github.com>
2025-07-05 20:34:07 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (73.40%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,23 +1,24 @@
 class Solution {
-    void backtrack(int idx, vector<int>& arr, int target, vector<int> temp, vector<vector<int>> &ans, int currsum){
+    void backtrack(int idx, vector<int>& arr, int target, vector<int>& temp, vector<vector<int>> &ans, int currsum){
         if(currsum==target){
             ans.push_back(temp);
             return;
         }
-        if(currsum>target || idx==arr.size()){
+        if(currsum>target || idx>=arr.size()){
             return;
         }
         temp.push_back(arr[idx]);
-        currsum+=arr[idx];
-        backtrack(idx,arr,target,temp,ans,currsum);
+        // currsum+=arr[idx];
+        backtrack(idx,arr,target,temp,ans,currsum+arr[idx]);
         temp.pop_back();
-        currsum-=arr[idx];
+        // currsum-=arr[idx];
         backtrack(idx+1,arr,target,temp,ans,currsum);
     }
 public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>> ans;
-        backtrack(0,candidates,target,{},ans,0);
+        vector<int> temp;
+        backtrack(0,candidates,target,temp,ans,0);
         return ans;
     }
 };
