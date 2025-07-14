dc68555c7c5955c98d69d73082e8e82022fc16b8
Prateek Mhatre <117933472+Prateek-1920@users.noreply.github.com>
2025-07-14 23:38:53 +0530

    Time: 12 ms (13.04%) | Memory: 19.5 MB (13.57%) - LeetSync

---
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        vector<vector<pair<int,int>>> graph(n);
+        vector<int> min_steps(n,INT_MAX);
+        min_steps[src] = 0;
+
+        for(auto &i : flights){
+            int src = i[0];
+            int dest = i[1];
+            int cost = i[2];
+            graph[src].push_back({dest,cost});
+        }
+
+        priority_queue<vector<int>, vector<vector<int>>, greater<>> pq;
+        pq.push({0,src,0});
+        while(!pq.empty()){
+            int node = pq.top()[1];
+            int cost = pq.top()[0];
+            int steps = pq.top()[2];
+            pq.pop();
+
+            if(node==dst){
+                return cost;
+            }
+            if(steps>k || steps > min_steps[node]){
+                continue;
+            }
+            min_steps[node] = steps;
+          
+            for(auto &it : graph[node]){
+                int neighbour = it.first;
+                int newcost = it.second + cost;
+                pq.push({newcost,neighbour,steps+1});
+            }
+        }
+        return -1;
+
+    }
+};
