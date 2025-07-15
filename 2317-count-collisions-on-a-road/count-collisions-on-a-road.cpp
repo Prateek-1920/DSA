fda68cf0a5877506f1591002389373232086d2cf
Prateek Mhatre <117933472+Prateek-1920@users.noreply.github.com>
2025-07-15 23:14:29 +0530

    Time: 7 ms (86.22%) | Memory: 19 MB (99.25%) - LeetSync

--- 2317-count-collisions-on-a-road/count-collisions-on-a-road.cpp
+++ 2317-count-collisions-on-a-road/count-collisions-on-a-road.cpp
@@ -1,40 +1,62 @@
+// class Solution {
+// public:
+//     int countCollisions(string directions) {
+//         stack<char>st;
+//         int count = 0;
+
+//         for(char ch : directions){
+//             if(!st.empty() && st.top()=='R' && ch =='L'){
+//                 count+=2;
+//                 st.pop();
+//                 st.push('S');
+//             }
+//             else if(!st.empty() && st.top()=='R' && ch =='S'){
+//                 count+=1;
+//                 st.pop();
+//                 st.push('S');
+//             }
+//             else if(!st.empty() && st.top()=='S' && ch=='L'){
+//                 count+=1;
+//                 // st.pop();
+//                 st.push('S');
+//             }
+//             else{
+//                 st.push(ch);
+//             }
+//         }
+//         while(!st.empty() && st.top()=='R'){
+//             st.pop();
+//         }
+
+//         while(!st.empty()){
+//             if(st.top()=='R'){
+//                 count++;
+//             }
+//             st.pop();
+//         }
+//         return count;
+
+//     }
+// };
+
+
 class Solution {
 public:
     int countCollisions(string directions) {
-        stack<char>st;
+        int n = directions.size();
         int count = 0;
 
-        for(char ch : directions){
-            if(!st.empty() && st.top()=='R' && ch =='L'){
-                count+=2;
-                st.pop();
-                st.push('S');
-            }
-            else if(!st.empty() && st.top()=='R' && ch =='S'){
-                count+=1;
-                st.pop();
-                st.push('S');
-            }
-            else if(!st.empty() && st.top()=='S' && ch=='L'){
-                count+=1;
-                // st.pop();
-                st.push('S');
-            }
-            else{
-                st.push(ch);
-            }
-        }
-        while(!st.empty() && st.top()=='R'){
-            st.pop();
-        }
+        // Ignore all cars moving away from the group at the beginning and end
+        int i = 0;
+        while (i < n && directions[i] == 'L') i++;
+        int j = n - 1;
+        while (j >= 0 && directions[j] == 'R') j--;
 
-        while(!st.empty()){
-            if(st.top()=='R'){
+        // Now from i to j, all moves cause collision except 'S'
+        for (int k = i; k <= j; k++) {
+            if (directions[k] != 'S')
                 count++;
-            }
-            st.pop();
         }
         return count;
-
     }
 };
