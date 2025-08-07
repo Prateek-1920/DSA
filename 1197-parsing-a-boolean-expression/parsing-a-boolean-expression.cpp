aff96a866e14f8a1fea41c68b829b7866ea154e7
Prateek Mhatre <117933472+Prateek-1920@users.noreply.github.com>
2025-08-08 00:22:10 +0530

    Time: 11 ms (27.88%) | Memory: 13.2 MB (22.29%) - LeetSync

---
+++ 1197-parsing-a-boolean-expression/parsing-a-boolean-expression.cpp
@@ -0,0 +1,49 @@
+class Solution {
+private:
+    bool parse(char op,vector<char>&vec){
+        if(op=='!'){
+            return vec[0] == 'f';
+        }
+    
+        else if(op=='&'){
+            for(auto c : vec){
+                if(c=='f')return false;
+            }
+            return true;
+        }
+        else{
+            for(auto c : vec){
+                if(c=='t')return true;
+            }
+            return false;
+        }
+    }
+   
+public:
+    bool parseBoolExpr(string expression) {
+        stack<char> st;
+
+        for(char c : expression){
+            if(c==','){
+                continue;
+            }
+            else if(c!=')'){
+                st.push(c);
+            }
+            else{
+                vector<char>temp;
+                while(!st.empty() && st.top()!='('){
+                    temp.push_back(st.top());
+                    st.pop();
+                }
+                reverse(temp.begin(),temp.end());
+                st.pop();
+                char op = st.top();
+                st.pop();
+                bool b = parse(op,temp);
+                st.push(b?'t':'f');
+            }
+        }
+        return st.top()=='t';
+    }
+};
