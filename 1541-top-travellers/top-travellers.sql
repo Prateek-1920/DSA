f4616ace785e9c447d78666c7b3baa8bf6f03b45
Prateek Mhatre <117933472+Prateek-1920@users.noreply.github.com>
2025-08-11 10:46:28 +0530

    Time: 820 ms (58.00%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1541-top-travellers/top-travellers.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+select u.name ,
+case
+when
+u.id in (select user_id from rides) then sum(r.distance)
+else 0
+end as travelled_distance  
+from users u left join rides r on 
+u.id = r.user_id
+group by r.user_id
+order by travelled_distance desc, u.name;
