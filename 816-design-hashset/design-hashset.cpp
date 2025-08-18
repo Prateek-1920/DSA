dbf65364293a729b0757122966f7519d7dd299e1
Prateek Mhatre <117933472+Prateek-1920@users.noreply.github.com>
2025-08-18 21:28:46 +0530

    Time: 15 ms (59.85%) | Memory: 48.4 MB (69.42%) - LeetSync

---
+++ 816-design-hashset/design-hashset.cpp
@@ -0,0 +1,46 @@
+class MyHashSet {
+public:
+    int M = 1000;
+    vector<list<int>> buckets;
+    MyHashSet() {
+        M = 1000;
+        buckets = vector<list<int>>(M,list<int>{});
+    }
+
+    int getindex(int key){
+        return key%M;
+    }
+    
+    void add(int key) {
+        int index = getindex(key);
+        auto itr = find(buckets[index].begin(),buckets[index].end(),key);
+        if(itr==buckets[index].end()){
+            buckets[index].push_back(key);
+        }
+    }
+    
+    void remove(int key) {
+        int index = getindex(key);
+        auto itr = find(buckets[index].begin(),buckets[index].end(),key);
+        if(itr!=buckets[index].end()){
+            buckets[index].erase(itr);
+        }
+    }
+    
+    bool contains(int key) {
+        int index = getindex(key);
+        auto itr = find(buckets[index].begin(),buckets[index].end(),key);
+        if(itr==buckets[index].end()){
+            return false;
+        }
+        return true;
+    }
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
