9734837501717fa27053cfac840aa6a3bfb0664c
Prateek Mhatre <117933472+Prateek-1920@users.noreply.github.com>
2026-09-17 22:37:44 +0530

    Time: 2 ms (12.72%) | Memory: 10.6 MB (5.41%) - LeetSync

---
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+    
+        vector<int>freq(26,0);
+
+        for(char c : word){
+            freq[c-'a']++;
+        }
+
+        sort(freq.rbegin(),freq.rend());
+        
+        int ans = 0;
+
+        for(int i=0;i<freq.size();i++){
+            ans += ((i/8)+1) * freq[i];
+        }
+
+        return ans;
+
+
+
+    }
+};
